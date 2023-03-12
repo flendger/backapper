8b5da0539f4448396d374c3dc1e20ccd61c9e020
kiru.e <evgeny.kiru@v6services.ru>
2023-03-12 20:32:19 +0300

    added backupcontroller

---
+++ app/backupcontroller/backupcontroller.go
@@ -0,0 +1,43 @@
+package backupcontroller
+
+import (
+	"backapper/app/appservice"
+	"log"
+	"net/http"
+)
+
+type BackupController struct {
+	service *appservice.AppService
+}
+
+func (c *BackupController) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	appName, exists := query["app"]
+	if !exists {
+		info := "Bad request: no app param"
+		writeResponse(info, response)
+		return
+	}
+
+	backUp, err := c.service.BackUp(appName[0])
+	if err != nil {
+		errInfo := "Back error: " + err.Error()
+		writeResponse(errInfo, response)
+		return
+	}
+
+	info := "OK: " + backUp
+	writeResponse(info, response)
+}
+
+func writeResponse(info string, response http.ResponseWriter) {
+	log.Println(info)
+	_, err := response.Write([]byte(info))
+	if err != nil {
+		return
+	}
+}
+
+func New(service *appservice.AppService) *BackupController {
+	return &BackupController{service: service}
+}
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"backapper/app/appreader"
+	"backapper/app/appservice"
+	"backapper/app/backupcontroller"
+	"log"
+	"net/http"
+)
 
+func main() {
+	appHolder := appreader.Read("apps.json")
+	service := appservice.New(appHolder)
+
+	backUpController := backupcontroller.New(service)
+
+	http.Handle("/backup", backUpController)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
